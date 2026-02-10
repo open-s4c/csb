d017a45a403bbb0b4348364b2d4919de3e0b69a5
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-02-10 17:20:10 +0200

    update
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-generator/test.sh
+++ bm-generator/test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
 # SPDX-License-Identifier: MIT
-
 set -e
 
 STRACE_LOG="ls.log"
-strace -o ${STRACE_LOG} -a 1 -s 65500 -v -xx -f -Xraw ls
+APP="ls"
+strace -o ${STRACE_LOG} -a 1 -s 65500 -v -xx -f -Xraw --raw=wait4 ${APP}
 
- ./00_init.sh
- ./01_build.sh
- ./02_parse.sh ${STRACE_LOG}
- ./03_extract.sh
- ./04_prepare.sh
- ./05_generate.sh
+./00_init.sh
+./01_build.sh
+./02_parse.sh ${STRACE_LOG}
+./03_extract.sh
+./04_prepare.sh
+./05_generate.sh
