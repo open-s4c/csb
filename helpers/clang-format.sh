db2efff838f8810a3cbb493f6f708d7772e670c9
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-02-13 17:45:02 +0200

    update
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- helpers/clang-format.sh
+++ helpers/clang-format.sh
@@ -5,25 +5,11 @@
 
 set -e
 
-if [ $# -lt 1 ]; then
-    echo "Usage: $0 <PATH> [PATH ...]"
-    echo ""
-    echo "  PATH                one or more directories (or source files) to recursively run "
-    echo "                      clang-format on."
-    echo "Environment variables:"
-    echo "  SILENT=true         disable git diff and error code"
-    echo "  STYLE=FILE          FILE is a configuration file, default = .clang-format"
-    echo "  "
-    echo ""
-    exit 1
-fi
-
 if [ "${STYLE}" != "" ]; then
     STYLE=":${STYLE}"
 fi
 
-git ls-files '*.h' '*.c' | grep -v 'deps' | grep -v 'bench/targets/syz/' |
-    xargs -I {} clang-format -style=file${STYLE} -i "$(pwd)/{}"
+git ls-files '*.h' '*.c' | grep -v 'deps' | grep -v 'bench/targets/syz/' | xargs -I {} clang-format -style=file${STYLE} -i "$(pwd)/{}"
 
 
 if [ "${SILENT}" != "true" ]; then
