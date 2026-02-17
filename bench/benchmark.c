70d85071e9263d859d220c977ec987831b2711bc
Bohdan Trach <bohdan.trach@huawei.com>
2026-02-17 17:32:54 +0100

    Allow configuring host and port at runtime.

--- bench/benchmark.c
+++ bench/benchmark.c
@@ -2,6 +2,19 @@
  * Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
  * SPDX-License-Identifier: MIT
  */
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdbool.h>
+
+static struct sockaddr_in bm_connect_addr;
+static bool bm_connect_addr_inited;
+
+static struct sockaddr_in bm_bind_addr;
+static bool bm_bind_addr_inited;
+
 #include <CSB/thread_launcher.h>
 #include <CSB/time.h>
 #include <CSB/bm_params.h>
@@ -12,7 +25,7 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include <stdbool.h>
+
 #define DISTRIBUTION_BOUND 1024
 #define BM_PRINT_DELIMITER ';'
 
@@ -110,6 +123,38 @@ run(void *args)
     return NULL;
 }
 
+static void
+parse_net_addr(const char *addr_env, const char *port_env,
+               struct sockaddr_in *dst, bool *inited)
+{
+    const char *conn_addr_str = getenv(addr_env);
+    const char *conn_port_str = getenv(port_env);
+    if (conn_addr_str) {
+        dst->sin_family = AF_INET;
+        dst->sin_port   = htons(31334);
+        int r =
+            inet_pton(dst->sin_family, conn_addr_str, &dst->sin_addr.s_addr);
+        if (r == 1) {
+            *inited = true;
+        } else if (r == -1) {
+            perror("inet_pton(connect addr)");
+            exit(-1);
+        } else {
+            fprintf(stderr,
+                    "inet_pton(connect addr): Not in presentation format.");
+            exit(-1);
+        }
+    }
+    if (*inited && conn_port_str) {
+        unsigned long port = strtoul(conn_port_str, NULL, 0);
+        if (errno == ERANGE) {
+            perror("strtoul(conn_port_str)");
+            exit(-1);
+        }
+        dst->sin_port = htons(port);
+    }
+}
+
 void
 bm_phase_warmup(void)
 {
@@ -147,6 +192,11 @@ bm_phase_warmup(void)
     for (size_t i = 0; i < DISTRIBUTION_BOUND; i++) {
         assert(g_ops[i] < g_params.op_dist_len);
     }
+
+    parse_net_addr("BM_SYS_CONNECT_ADDR", "BM_SYS_CONNECT_PORT",
+                   &bm_connect_addr, &bm_connect_addr_inited);
+    parse_net_addr("BM_SYS_BIND_ADDR", "BM_SYS_BIND_PORT", &bm_bind_addr,
+                   &bm_bind_addr_inited);
 }
 
 void
