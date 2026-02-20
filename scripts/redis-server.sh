e670471e80fb18df407fe25623b44113e42fcc4d
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-02-20 14:43:35 +0200

    update missing line
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- scripts/redis-server.sh
+++ scripts/redis-server.sh
@@ -2,7 +2,6 @@
 # Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
 # SPDX-License-Identifier: MIT
 
-
 duration="$1"
 port_offset="$2"
 timeout $1 redis-server --port $((port_offset + 8000)) --protected-mode no || true
