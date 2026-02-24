8fd189ff7cee6029fbc3e0d64eb8ba526ee4011a
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-02-24 16:43:35 +0200

    update
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/monitors/monitor_factory.py
+++ bm-runner/monitors/monitor_factory.py
@@ -9,11 +9,30 @@ from monitors.sarnet import SarNetStats
 from monitors.monitor import Monitor
 from utils.logger import bm_log, LogType
 import sys
+from config.env_config import EnvUniversalConfig, UniversalConfig
 
+class DummyMonitor(Monitor):
+    def __init__(self, name:str):
+        super().__init__(dir="", args=[])
+        self.name = name
+
+    def start(self):
+        bm_log(f"Requested monitor `{self.name}` from config is not started, since environment variable `{UniversalConfig.CSB_ANALYZE}` "
+               f"is set to `false`."
+               f"To reactivate monitors remove `{UniversalConfig.CSB_ANALYZE}` or set it to `true`", LogType.WARNING)
+    def stop(self):
+        pass
+    def collect_results(self) -> str:
+        return ""
 
 class MonitorFactory:
     @staticmethod
     def create(monitor_type: MonitorType, results_dir, args) -> Monitor:
+        # if the user has requested to disable the monitors,
+        # we return a dummy monitor that does nothing,
+        # so that the rest of the code can remain unchanged
+        if not EnvUniversalConfig.is_on(UniversalConfig.CSB_ANALYZE):
+            return DummyMonitor(name=f"{monitor_type}")  # Return a dummy monitor that does nothing
         match monitor_type:
             case MonitorType.MPSTAT:
                 return SystemStats(output_dir=results_dir, args=args)
