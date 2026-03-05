839f086daa675a88fbff82f000dbc031b51ed0db
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-03-05 15:42:34 +0200

    update
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

---
+++ scripts/plugins/generic-network-client.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <PORT> <CLIENT_PATH> <META_PATH>"
+    exit 1
+fi
+
+PORT=$1
+CLIENT_PATH=$2
+META_PATH=$3
+IP="127.0.0.0"
+
+META_STRING=$(cat "$META_PATH")
+
+echo "${META_STRING}"
+${CLIENT_PATH} -h ${IP} -p${PORT} -P${META_STRING}
