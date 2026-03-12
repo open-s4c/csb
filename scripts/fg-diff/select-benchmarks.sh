6e14ae7c9eb68ed2dbb4f8dfdf06ce8b5d71a9ea
Bohdan Trach <bohdan.trach@huawei.com>
2026-03-12 11:32:05 +0100

    Select benchmarks after the template generation.
    Signed-off-by: Bohdan Trach <bohdan.trach@huawei.com>

--- scripts/fg-diff/select-benchmarks.sh
+++ scripts/fg-diff/select-benchmarks.sh
@@ -9,16 +9,29 @@ if [ ! -d results ] || [ ! -d bm-runner ] || [ ! -d ./scripts/fg-diff ] || [ ! -
 fi
 export FLAMEGRAPH="$(realpath ./deps/FlameGraph)"
 
+mkdir -p logs
+
 export CSB_NO_CLEAN_BENCH=1
 for i in $*; do
-    ./scripts/fg-diff/run-single.sh $(realpath "$i")
+    fbase=$(basename $i .json)
+    ./scripts/fg-diff/run-single.sh $(realpath "$i") > ./logs/$fbase.stdout 2> ./logs/$fbase.stderr || true
 done
 
 source ./venv/bin/activate
 
+RESULTS_DIR="./results"
+if [ ! -z "CSB_RESULTS_GROUP" ]; then
+    RESULTS_DIR="./results/$CSB_RESULTS_GROUP"
+fi
+
 mkdir -p ./bench-select
 echo "Preprocessing perf.data files from benchmark results"
-./scripts/fg-diff/filter-all.sh ./results/ ./bench-select
+./scripts/fg-diff/filter-all.sh "$RESULTS_DIR" ./bench-select
 echo "Calculating difference between flamegraphs"
 ./scripts/fg-diff/diff-all.sh ./bench-select > ./bench-select/diff.csv
-python3 ./scripts/fg-diff/diffset.py --cutoff 5 --input ./bench-select/diff.csv
+
+if [ ! -z "$CSB_SELECTED_OUTPUT" ]; then
+    python3 ./scripts/fg-diff/diffset.py --cutoff 5 --input ./bench-select/diff.csv > "$CSB_SELECTED_OUTPUT"
+else
+    python3 ./scripts/fg-diff/diffset.py --cutoff 5 --input ./bench-select/diff.csv
+fi
