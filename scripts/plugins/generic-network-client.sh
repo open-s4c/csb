open-s4c/csb

commit 4b54e0b61eaefb2d316940e85d83cb1672985ba1

Fix `mysql-aggregated.json` (#108)
Fix

- `mysql-aggregated.json` pass all meta files to networking script
- let networking script accept a list of meta files

---------

Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

scripts/plugins/generic-network-client.sh
@@ -2,31 +2,42 @@
 # Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
 # SPDX-License-Identifier: MIT
 
-if [ $# -ne 4 ]; then
-    echo "Usage: $0 <PORT> <CLIENT_COUNT> <CLIENT_PATH> <META_PATH>"
+if [ $# -lt 4 ]; then
+    echo "Usage: $0 <PORT> <CLIENT_COUNT> <CLIENT_PATH> <META_PATH#1> <META_PATH#2> ..."
     exit 1
 fi
 
+IP="127.0.0.1"
 START_PORT=$1
 CLIENT_COUNT=$2
 CLIENT_PATH=$3
-META_PATH=$4
-IP="127.0.0.0"
 
-# grep everything after `=` and between quotes `"..."`
-# WARNING multiple sequences "", "", "" are not handled
-META_STRING=$(grep '^SERVER_SEQ=' $META_PATH | sed 's/^SERVER_SEQ="\([^"]*\)"/\1/')
-echo $META_STRING
+# Shift away the first 3 arguments
+shift 3
+
+# Remaining arguments are the meta paths
+META_PATHS=("$@")
+
+# Validate count
+if [ "${#META_PATHS[@]}" -ne "$CLIENT_COUNT" ]; then
+    echo "Error: Expected $CLIENT_COUNT meta paths, got ${#META_PATHS[@]}"
+    exit 1
+fi
+
 # TODO: check if meta string is empty
 # TODO: create an analogous case for CLIENT_SEQ
 # TODO: make it decide whether to launch a client or a server based on
 # the available sequence
 for ((i=0; i<$CLIENT_COUNT; i++)); do
     port=$((START_PORT + i))
-    echo "Launching client $i on port $port"
+    metafile="${META_PATHS[$i]}"
+    # grep everything after `=` and between quotes `"..."`
+    # WARNING multiple sequences "", "", "" are not handled
+    META_STRING=$(grep '^SERVER_SEQ=' $metafile | sed 's/^SERVER_SEQ="\([^"]*\)"/\1/')
+    echo "Launching client#$i on port $port, with meta $metafile"
     $CLIENT_PATH "-R" "-h" "$IP" "-p$port" "-P$META_STRING" &
 done
 
-echo "\nwaiting for clients to finish..."
+echo "Waiting for clients to finish..."
 wait
 echo "Clients are done"

scripts/plugins/launch-clients-local.sh
@@ -30,6 +30,6 @@ for ((i=0; i<$CLIENT_COUNT; i++)); do
     $CLIENT_PATH/network/redis-client "$IP" "$port" &
 done
 
-echo "\nwaiting for clients to finish..."
+echo "Waiting for clients to finish..."
 wait
 echo "Clients are done"

commit 82af8291d859eaaa95e98ee018799298a0b6595c

[plugins] Fix networking script (#116)
Fix

- instead of an error if number of clients don't match meta
   repeat the last meta for the remaining clients


Note

- this is issue was introduced in #108 , which
  fixed the script for the aggregated case and introduced
  an issue for single bm case. This fix should work for both.

Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

scripts/plugins/generic-network-client.sh
@@ -18,10 +18,14 @@ shift 3
 # Remaining arguments are the meta paths
 META_PATHS=("$@")
 
-# Validate count
-if [ "${#META_PATHS[@]}" -ne "$CLIENT_COUNT" ]; then
-    echo "Error: Expected $CLIENT_COUNT meta paths, got ${#META_PATHS[@]}"
-    exit 1
+# If fewer meta paths than CLIENT_COUNT, repeat the last one
+if [ "${#META_PATHS[@]}" -lt "$CLIENT_COUNT" ]; then
+    echo "[Warning] Expected $CLIENT_COUNT meta paths, got ${#META_PATHS[@]}"
+    last="${META_PATHS[-1]}"
+    while [ "${#META_PATHS[@]}" -lt "$CLIENT_COUNT" ]; do
+        META_PATHS+=("$last")
+    done
+    echo "[Warning] Repeating last meta to compensate!"
 fi
 
 # TODO: check if meta string is empty