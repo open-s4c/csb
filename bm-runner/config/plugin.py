54e47a4f0cfde06e8f323c756869b61df1780310
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-03-31 18:16:02 +0300

    document and refactor
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/config/plugin.py
+++ bm-runner/config/plugin.py
@@ -11,6 +11,7 @@ from typing import Optional
 from utils.logger import bm_log, LogType
 from bm_utils import ensure_exists
 from pathlib import Path
+from utils.process import BackgroundProcess
 
 
 class ExecutionTime(str, Enum):
@@ -85,35 +86,23 @@ class Plugin(dict):
         return " ".join(commands)
 
     def execute(self, results_dir, **kwargs):
-        tmpfile = tempfile.NamedTemporaryFile(dir=results_dir, delete=False)
         commands = [self.fname]
         if self.args is not None:
             commands.extend(map(lambda arg: arg.format(**kwargs), self.args))
+        self.process = BackgroundProcess(name=self.name, out_dir=results_dir, cmds=commands)
+        self.process.start()
+        self.check()
 
-        self.process = subprocess.Popen(
-            commands, stdout=tmpfile, stderr=tmpfile, preexec_fn=os.setpgrp
-        )
-        if self.check():
-            bm_log(f"Launched {' '.join(commands)} -> Output file: {tmpfile.name}")
-
-    def check(self) -> bool:
+    def check(self):
         # with failure will be detected with the app itself
-        if self.exec_time == ExecutionTime.WITH:
-            return True
-        if self.process is None:
-            return True
-        rc = self.process.poll()
-        if rc is None or rc == 0:
-            return True
-        bm_log(f"Plugin: {self.name} failed with error code {rc}.", LogType.FATAL)
-        sys.exit(1)
-        return False
+        if self.exec_time == ExecutionTime.WITH or self.process is None:
+            return
+        if not self.process.is_alive():
+            sys.exit(1)
 
     def stop(self):
         if self.process is not None:
             if self.force_stop:
-                bm_log(f"Killing {self.name}, with PID = {self.process.pid}")
-                stop_process(self.process.pid)
+                self.process.force_stop()
             else:
-                bm_log(f"Waiting for {self.name}, with PID = {self.process.pid} to terminate")
-                self.process.wait()
+                self.process.wait_indefinitely()
