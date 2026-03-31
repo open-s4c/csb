ba93a1313fbef0a44066e58877cd935e33d3a8aa
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-03-31 16:00:18 +0300

    cleanup
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/monitors/sarnet.py
+++ bm-runner/monitors/sarnet.py
@@ -12,39 +12,16 @@ from bm_utils import ensure_exists
 from utils.logger import bm_log
 from utils.process import BackgroundProcess
 
-
-class SarCmd:
-    def __init__(self, output_dir: str, cmd_args: list[str] = []):
-        cmds = ["sudo", "ip", "netns", "exec"]
-        cmds.append(cmd_args[0])
-        cmds.extend(["sar", "-n", "DEV,EDEV", "-o", "netstats.sar"])
-        cmds.extend(["--iface={}".format(cmd_args[1]), "1"])
-        cmd_str = " ".join(cmds)
-        bm_log(f"Running sar: {cmd_str}")
-        self.process = subprocess.Popen(
-            cmds,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-            preexec_fn=os.setpgrp,
-            cwd=output_dir,
-        )
-
-    def stop(self):
-        # This acts like ctrl+C
-        self.process.send_signal(signal.SIGINT)
-        self.process.wait()
-
-
 class SarNetStats(Monitor):
     def __init__(self, output_dir: str, args: list[str] = []):
+        super().__init__(dir=output_dir, args=args)
         for tool in ["sar", "sadf"]:
             ensure_exists(tool)
-        super().__init__(dir=output_dir, args=args)
         cmds = ["sudo", "ip", "netns", "exec"]
         cmds.append(args[0])
         cmds.extend(["sar", "-n", "DEV,EDEV", "-o", "netstats.sar"])
         cmds.extend(["--iface={}".format(args[1]), "1"])
-        self.sar = BackgroundProcess(name=args[0], out_dir=output_dir, cmds=cmds)
+        self.sar = BackgroundProcess(name="SarNetStats", out_dir=output_dir, cmds=cmds, check_exists=False)
 
     def start(self):
         self.sar.start()
@@ -142,3 +119,4 @@ class SarNetStats(Monitor):
 
     def stop(self):
         self.sar.stop()
+        self.plot()
