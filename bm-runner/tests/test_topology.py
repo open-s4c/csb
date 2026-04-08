ee393eaf5abebd471f8ed644ee34a64002491c90
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-04-08 17:32:24 +0300

    Update
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/tests/test_topology.py
+++ bm-runner/tests/test_topology.py
@@ -4,6 +4,9 @@ from benchkit.shell.shell import shell_out
 import pytest
 
 
+# To be able to test different outputs
+# we mock the output of lscpu by using
+# existing files
 def __read(file) -> list[str]:
     cpu_info = shell_out(
         f"cat tests/{file}",
@@ -14,10 +17,12 @@ def __read(file) -> list[str]:
     return lines
 
 
+# use hyper threading lscpu mock
 def __mock_read_info(self) -> list[str]:
     return __read("lscpu_mock.csv")
 
 
+# use no hyper threading lscpu mock
 def __mock_read_info_no_ht(self) -> list[str]:
     return __read("lscpu_mock_no_ht.csv")
 
@@ -32,24 +37,33 @@ def mock_read_info_no_ht(mocker):
     return mocker.patch.object(Topology, "_Topology__read_info", __mock_read_info_no_ht)
 
 
+# ensure on default setting we get
+# the requested count of cpus
 def test_default_count(mock_read_info):
     topology = Topology()
     policy = CoreAssignPolicy()
     count = 1000
-    cpu_list = topology.select(count=count, policy=policy)
-    assert len(cpu_list) == count, "Less CPUs returned than requested!"
+    for count in 1000, 1:
+        cpu_list = topology.select(count=count, policy=policy)
+        assert len(cpu_list) == count, "Less CPUs returned than requested!"
 
 
+# ensure we get the requested count of cpus
+# if a preselected list is provided
 def test_pre_select_count(mock_read_info):
     topology = Topology()
     policy = CoreAssignPolicy()
     cores = [1, 5, 6]
-    count = 10
-    cpu_list = topology.select(count=count, policy=policy, pre_selected=cores)
-    assert len(cpu_list) == count, "Less CPUs returned than requested!"
-    assert set(cpu_list) == set(cores), "Unexpected cores are used"
+    for count in 10, 1:
+        cpu_list = topology.select(count=count, policy=policy, pre_selected=cores)
+        assert len(cpu_list) == count, "Less CPUs returned than requested!"
+        if count > len(cores):
+            assert set(cpu_list) == set(cores), "Unexpected cores are used"
 
 
+# check counts match expectation
+# the expectation depends on the mock file
+# with hyper-threading
 def test_stats(mock_read_info):
     topology = Topology()
     exp_cpu_count = 320
@@ -73,11 +87,17 @@ def verify_lists(topo: Topology):
     assert exp_pkg_list == topo.get_packages()
 
 
+# check lists match expectation
+# the expectation depends on the mock file
+# with hyper-threading
 def test_lists(mock_read_info):
     topology = Topology()
     verify_lists(topology)
 
 
+# check counts match expectation
+# the expectation depends on the mock file
+# without hyper-threading
 def test_stats_no_ht(mock_read_info_no_ht):
     topology = Topology()
     exp_cpu_count = 256
@@ -90,6 +110,9 @@ def test_stats_no_ht(mock_read_info_no_ht):
     assert exp_pkg_count == topology.get_package_count()
 
 
+# check lists match expectation
+# the expectation depends on the mock file
+# without hyper-threading
 def test_lists_no_ht(mock_read_info_no_ht):
     topology = Topology()
     verify_lists(topology)
