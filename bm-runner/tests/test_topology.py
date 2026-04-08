a3aac29031b0a062850dd0b631e855bffb04b879
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-04-08 17:20:00 +0300

    testing in progress
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/tests/test_topology.py
+++ bm-runner/tests/test_topology.py
@@ -1,28 +1,38 @@
 from utils.topology import Topology
-from config.policy import CoreAssignPolicy, PackGroup, CpuOrder
+from config.policy import CoreAssignPolicy
 from benchkit.shell.shell import shell_out
-from pathlib import Path
-import os
-from unittest.mock import patch
 import pytest
 
-def __mock_read_info(self) -> list[str]:
-    print(csb_dir())
+
+def __read(file) -> list[str]:
     cpu_info = shell_out(
-        "cat tests/lscpu_mock.csv",
+        f"cat tests/{file}",
         print_output=False,
         print_file_shell_cmd=False,
     )
     lines = cpu_info.strip().split("\n")
-    print("mock method is called")
     return lines
+
+
+def __mock_read_info(self) -> list[str]:
+    return __read("lscpu_mock.csv")
+
+
+def __mock_read_info_no_ht(self) -> list[str]:
+    return __read("lscpu_mock_no_ht.csv")
+
 
 @pytest.fixture
 def mock_read_info(mocker):
-    return mocker.patch.object(Topology, '_Topology__read_info', __mock_read_info)
+    return mocker.patch.object(Topology, "_Topology__read_info", __mock_read_info)
 
 
-def test_default(mock_read_info):
+@pytest.fixture
+def mock_read_info_no_ht(mocker):
+    return mocker.patch.object(Topology, "_Topology__read_info", __mock_read_info_no_ht)
+
+
+def test_default_count(mock_read_info):
     topology = Topology()
     policy = CoreAssignPolicy()
     count = 1000
@@ -30,3 +40,56 @@ def test_default(mock_read_info):
     assert len(cpu_list) == count, "Less CPUs returned than requested!"
 
 
+def test_pre_select_count(mock_read_info):
+    topology = Topology()
+    policy = CoreAssignPolicy()
+    cores = [1, 5, 6]
+    count = 10
+    cpu_list = topology.select(count=count, policy=policy, pre_selected=cores)
+    assert len(cpu_list) == count, "Less CPUs returned than requested!"
+    assert set(cpu_list) == set(cores), "Unexpected cores are used"
+
+
+def test_stats(mock_read_info):
+    topology = Topology()
+    exp_cpu_count = 320
+    assert exp_cpu_count == topology.get_cpu_count()
+    exp_core_count = 160
+    assert exp_core_count == topology.get_core_count()
+    exp_numa_count = 4
+    assert exp_numa_count == topology.get_numa_count()
+    exp_pkg_count = 2
+    assert exp_pkg_count == topology.get_package_count()
+
+
+def verify_lists(topo: Topology):
+    exp_cpu_list = list(range(0, topo.get_cpu_count()))
+    assert exp_cpu_list == topo.get_cpus()
+    exp_cores_list = list(range(0, topo.get_core_count()))
+    assert exp_cores_list == topo.get_cores()
+    exp_numa_list = list(range(0, topo.get_numa_count()))
+    assert exp_numa_list == topo.get_numas()
+    exp_pkg_list = list(range(0, topo.get_package_count()))
+    assert exp_pkg_list == topo.get_packages()
+
+
+def test_lists(mock_read_info):
+    topology = Topology()
+    verify_lists(topology)
+
+
+def test_stats_no_ht(mock_read_info_no_ht):
+    topology = Topology()
+    exp_cpu_count = 256
+    assert exp_cpu_count == topology.get_cpu_count()
+    exp_core_count = 256
+    assert exp_core_count == topology.get_core_count()
+    exp_numa_count = 2
+    assert exp_numa_count == topology.get_numa_count()
+    exp_pkg_count = 2
+    assert exp_pkg_count == topology.get_package_count()
+
+
+def test_lists_no_ht(mock_read_info_no_ht):
+    topology = Topology()
+    verify_lists(topology)
--- bm-runner/utils/topology.py
+++ bm-runner/utils/topology.py
@@ -9,20 +9,16 @@ import itertools
 import numpy as np
 from typing import Optional
 import sys
-import os
 
-class TopologyCounts:
-    num_numas: int
-    num_cpus: int
-    num_cores: int
-    num_packages: int
 
 class Filter:
     group_name: str  # "Node"
     idx: int  # 0
+
     def __init__(self, name, idx):
         self.group_name = name
         self.idx = idx
+
 
 class Topology:
     CPU = "CPU"
@@ -33,23 +29,11 @@ class Topology:
     L3_CACHE = "L3"
     NUMA = "Node"
     PACKAGE = "Socket"
-    CLUSTER = "Cluster"  # careful mostly empty
+    CLUSTER = "Cluster"  # empty column
 
     def __init__(self):
         lines = self.__read_info()
         self.data = self.__transform_info(lines)
-
-    def get_counts(self) -> TopologyCounts:
-        stats = TopologyCounts()
-        stats.num_cpus = self.data[self.CPU].unique().size
-        stats.num_cores = self.data[self.CORE].unique().size
-        stats.num_numas = self.data[self.NUMA].unique().size
-        stats.num_packages = self.data[self.PACKAGE].unique().size
-        bm_log(f"#CPUS: {stats.num_cpus }")
-        bm_log(f"#CORES: {stats.num_cores}")
-        bm_log(f"#NUMAS: {stats.num_numas}")
-        bm_log(f"#PACKAGES: {stats.num_packages}")
-        return stats
 
     def __transform_info(self, lines: list[str]) -> pd.DataFrame:
         # Extract column header line
@@ -104,8 +88,6 @@ class Topology:
         max_cpu = max(pre_selected)
         cpu_count = max(self.data[self.CPU]) + 1
 
-        assert cpu_count == os.cpu_count()
-
         if max_cpu >= cpu_count:
             bm_log(
                 f"User want to assign to none existing CPU(s) {max_cpu}. Falling back to available CPUs",
@@ -139,3 +121,27 @@ class Topology:
             count=count, filter=filter, one_per_core=policy.one_cpu_per_core, desc=desc
         )
         return cpus
+
+    def get_numas(self) -> list[int]:
+        return self.data[self.NUMA].unique().tolist()
+
+    def get_numa_count(self) -> int:
+        return len(self.get_numas())
+
+    def get_packages(self) -> list[int]:
+        return self.data[self.PACKAGE].unique().tolist()
+
+    def get_package_count(self) -> int:
+        return len(self.get_packages())
+
+    def get_cores(self) -> list[int]:
+        return self.data[self.CORE].unique().tolist()
+
+    def get_core_count(self) -> int:
+        return len(self.get_cores())
+
+    def get_cpus(self) -> list[int]:
+        return self.data[self.CPU].unique().tolist()
+
+    def get_cpu_count(self) -> int:
+        return len(self.get_cpus())
