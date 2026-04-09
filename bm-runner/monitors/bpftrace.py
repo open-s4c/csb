7f7bcd1f0d91335403ae5da3a32b59e3201ee3ce
Martin Beck <martin.beck2@gmx.de>
2026-04-09 16:53:11 +0200

    test
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- bm-runner/monitors/bpftrace.py
+++ bm-runner/monitors/bpftrace.py
@@ -17,6 +17,7 @@ from typing import Optional
 
 class BPFTraceCmd:
     def continue_writing(self, stream, filename):
+        print(filename)
         try:
             with open(filename, "w") as f:
                 for line in stream:   # ← exits automatically on EOF
