3ec7a591f5ed0a1be505edf71951444d29c86557
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-05-11 17:17:45 +0300

    generate only for specific targets
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-generator/05_generate.sh
+++ bm-generator/05_generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
 # SPDX-License-Identifier: MIT
-cmake --build ../build --target csb-generate
+cmake --build ../build --target syz_single.h.in
+cmake --build ../build --target bm_single.json.in
