f8b0f2e90ab3d2d5d6e80dfb2c67fbead77c98d9
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-05-13 18:13:50 +0300

    throuput avg + success percent avg
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/analyze.py
+++ bm-runner/analyze.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 import os
 import pandas as pd
+from utils.logger import bm_log, LogType
+import sys
+
+group_by_fields : list[str] = ['algo_name', 'execution_type', 'hostname', 'kernel', 'nb_threads']
+avg_fields: list[str] = ['univ_succ_percent', 'throughput_min']
+
 
 # collect all csvs in the given folder
-def find_csvs(folder_path:str):
-    csvs = []
-    for root, dirs, files  in os.walk(folder_path):
+def get_all_files_by_ext(dir:str, extension:str = ".csv"):
+    """
+    Returns all files under the given dir and subdirs, which have the given file extension.
+    """
+    filtered_files = []
+    for root, _, files  in os.walk(dir):
         for file in files:
-            if file.endswith(".csv"):
-                file_path = os.path.join(root, file)
-                csvs.append(file_path)
-    return csvs
+            if file.endswith(extension):
+                abs_path = os.path.join(root, file)
+                filtered_files.append(abs_path)
+    return filtered_files
 
 # Allowed CPUs
 # kernel
@@ -26,13 +35,13 @@ def process(file:str) -> list:
         df = pd.read_csv(
             file, sep=";", comment="#", engine="python", on_bad_lines="error"
         )
-        grouped = df.groupby(['algo_name', 'execution_type', 'kernel', 'nb_threads'])
-        for key, g in grouped:
-            algo_name, execution_type, kernel, num_threads = key
+        grouped = df.groupby(group_by_fields)
+        for key_group, g in grouped:
+            per_run = g.groupby('container_cnt')
 
-            avg_per_container = g.groupby('container_cnt')['throughput_min'].mean()
+            avg_per_container = per_run['throughput_min'].mean()
             min_container     = avg_per_container.index.min()
-            avg_min_throughput = avg_per_container[min_container]
+            avg_min_throughput = float(avg_per_container[min_container])
             linearity_per_count = avg_per_container / avg_min_throughput
 
             dropped = linearity_per_count[(linearity_per_count < (1 - tolerance))]
@@ -43,23 +52,25 @@ def process(file:str) -> list:
                 checkmark = "✘"
                 drop_note = str(dropped.index.min())
 
-            results.append({
-                'Benchmark': algo_name,
-                'Execution Unit': execution_type,
-                'Kernel': kernel,
-                'Number of Threads': num_threads,
-                'linear': checkmark,
-                'Drops at': drop_note
-            })
 
-        return results
+            record = {group_by_fields[idx]: value for idx, value in enumerate(key_group)}
+            record['linear'] = checkmark
+            record['Drops at'] = drop_note
+            record['throughput_avg'] = avg_per_container.mean()
+
+            univ_succ_avg = per_run['univ_succ_percent'].mean()  # Series of container averages
+            record['univ_succ_percent'] = univ_succ_avg.mean() # scalar average across containers
+
+            results.append(record)
     except Exception as e:
-        print(f"{e} on {file}")
-        return results
+        bm_log(f"{e} on {file}", LogType.ERROR)
+
+    return results
 
 if __name__ == "__main__":
     folder = "/home/lilith/workspace/csb-analyze"
-    files = find_csvs(folder)
+    files = get_all_files_by_ext(folder)
+    bm_log(f"Running analysis on {folder}. {len(files)} CSV files detected ...", LogType.INFO)
     all = []
     for f in files:
         res = process(f)
