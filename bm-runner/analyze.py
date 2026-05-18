205cf0efbbb3530dd9d2a2d7e42a64cba2516d42
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-05-18 17:08:13 +0300

    update
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/analyze.py
+++ bm-runner/analyze.py
@@ -4,6 +4,8 @@
 import os
 import pandas as pd
 from utils.logger import bm_log, LogType
+from bm_visualize import plot_chart, create_mean_plot
+from config.plot import PlotConfig, PlotType
 import sys
 
 # TODO: maybe it is best to compare with container count
@@ -43,31 +45,15 @@ def process(file:str) -> list:
         )
         grouped = df.groupby(group_by_fields)
         for key_group, g in grouped:
-            per_run = g.groupby('container_cnt')
-
-            avg_per_container = per_run['throughput_min'].mean()
-            min_container     = avg_per_container.index.min()
-            avg_min_throughput = float(avg_per_container[min_container])
-            linearity_per_count = avg_per_container / avg_min_throughput
-
-            dropped = linearity_per_count[(linearity_per_count < (1 - tolerance))]
-            if dropped.empty:
-                checkmark = "✔"
-                drop_note = ""
-            else:
-                checkmark = "✘"
-                drop_note = str(dropped.index.min())
+            per_run = g.groupby('container_cnt').agg(
+                throughput_avg=('throughput_min', 'mean'),
+                univ_succ_percent=('univ_succ_percent', 'mean')
+            ).reset_index()  # container_cnt becomes a column
 
 
-            record = {group_by_fields[idx]: value for idx, value in enumerate(key_group)}
-            record['linear'] = checkmark
-            record['Drops at'] = drop_note
-            record['throughput_avg'] = avg_per_container.mean()
-
-            univ_succ_avg = per_run['univ_succ_percent'].mean()  # Series of container averages
-            record['univ_succ_percent'] = univ_succ_avg.mean() # scalar average across containers
-
-            results.append(record)
+            for idx, col in enumerate(group_by_fields):
+                per_run[col] = key_group[idx]
+            results.append(per_run)
     except Exception as e:
         bm_log(f"{e} on {file}", LogType.ERROR)
 
@@ -77,29 +63,36 @@ def gen_compare_summary(df) -> str:
     benchmarks = df['algo_name'].unique()
     benchmarks.sort()
     summary : list[str] = []
+    # get all results mapped to a certain benchmark
     for bm in benchmarks:
         # Filter the DataFrame to only this benchmark
         bm_summary = f"## {bm}\n\n"
         bm_df = df[df['algo_name'] == bm]
 
-        bm_summary += "| Kernel | Host | Type | Throughput Avg | Success Percent |\n"
-        bm_summary += "|--------|------|------|----------------|----------------|\n"
+        cfg =  PlotConfig(hue="kernel", y="throughput_avg", x="container_cnt")
+        plot_chart(cfg, bm_df, f"{bm}.png")
+        create_mean_plot(bm_df, cfg, dir=".")
+
+
+        bm_summary += "|container_cnt| Kernel | Host | Type | Throughput Avg | Success Percent |\n"
+        bm_summary += "|--------|--------|------|------|----------------|----------------|\n"
         # Add a row per configuration
         for _, row in bm_df.iterrows():
-            bm_summary += f"| {row['kernel']} | {row['hostname']} | {row['execution_type']} | {row['throughput_avg']:.2f} | {row['univ_succ_percent']:.1f} |\n"
+            bm_summary += f"|{row['container_cnt']}| {row['kernel']} | {row['hostname']} | {row['execution_type']} | {row['throughput_avg']:.2f} | {row['univ_succ_percent']:.1f} |\n"
 
-        # Determine the best throughput
-        max_throughput = bm_df['throughput_avg'].max()
-        best_rows = bm_df[bm_df['throughput_avg'] == max_throughput]
+        # # Determine the best throughput
+        # max_throughput = bm_df['throughput_avg'].max()
+        # best_rows = bm_df[bm_df['throughput_avg'] == max_throughput]
 
-        # List all rows that have the max throughput (there could be ties)
-        best_text = ", ".join(
-            f"{r['kernel']}/{r['hostname']}/{r['execution_type']}" for _, r in best_rows.iterrows()
-        )
-        bm_summary += f"\n**Best throughput:** {max_throughput:.2f} ({best_text})\n"
+        # # List all rows that have the max throughput (there could be ties)
+        # best_text = ", ".join(
+        #     f"{r['kernel']}/{r['hostname']}/{r['execution_type']}" for _, r in best_rows.iterrows()
+        # )
+        # bm_summary += f"\n**Best throughput:** {max_throughput:.2f} ({best_text})\n"
 
         # Add this benchmark summary to the list
         summary.append(bm_summary)
+
 
     # Join all benchmark summaries into one string
     return "\n".join(summary)
@@ -113,9 +106,10 @@ if __name__ == "__main__":
         res = process(f)
         all.extend(res)
 
-    final_df = pd.DataFrame(all)
+    final_df =  pd.concat(all, ignore_index=True)
 
     md_table = final_df.to_markdown(index=False)
+
     per_bm = gen_compare_summary(final_df)
 
 
@@ -123,6 +117,6 @@ if __name__ == "__main__":
     csv  = final_df.to_csv(index=False)
     with open("results.md", "w") as f:
          f.write(md_table)
-         f.write(per_bm)
+        # f.write(per_bm)
     with open("results.csv", "w") as f:
          f.write(csv)
